c7cd2ee03ae085840d82b513c4a5108a9bb70647
Matthias Albrecht <matthias.albrecht@uni-konstanz.de>
2025-08-21 23:13:52 +0200

    Improve rotation center UI

--- Assets/Scripts/RotationCenterHeight.cs
+++ Assets/Scripts/RotationCenterHeight.cs
@@ -5,37 +5,75 @@ using UnityEngine.UI;
 using TMPro;
 public class RotationCenterHeight : MonoBehaviour
 {
-    public float height = 0.088f;
+    public Vector3 screenRotationCenter = new Vector3(0, 0.088f, 0);
+    public Vector3 roomRotationCenter = new Vector3(0, 0.088f, 0);
 
-    public Transform roomCenter;
     public Transform screenCenter;
+    public Transform roomCenter;
 
     [Header("UI")]
-    public TMP_InputField heightInputField;
+    public TMP_InputField screenInputFieldX;
+    public TMP_InputField screenInputFieldY;
+    public TMP_InputField screenInputFieldZ;
+    [Space]
+    public TMP_InputField roomInputFieldX;
+    public TMP_InputField roomInputFieldY;
+    public TMP_InputField roomInputFieldZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        SetHeight(height);
+        SetScreenRotationCenter(screenRotationCenter);
+        SetRoomRotationCenter(roomRotationCenter);
 
-        heightInputField.SetTextWithoutNotify(height.ToString());
-        heightInputField.onEndEdit.AddListener(value =>
+        screenInputFieldX.SetTextWithoutNotify(screenRotationCenter.x.ToString());
+        screenInputFieldX.onEndEdit.AddListener(value =>
         {
-            SetHeight(float.Parse(value));
+            SetScreenRotationCenter(new Vector3(float.Parse(value), screenRotationCenter.y, screenRotationCenter.z));
+        });
+        screenInputFieldY.SetTextWithoutNotify(screenRotationCenter.y.ToString());
+        screenInputFieldY.onEndEdit.AddListener(value =>
+        {
+            SetScreenRotationCenter(new Vector3(screenRotationCenter.x, float.Parse(value), screenRotationCenter.z));
+        });
+        screenInputFieldZ.SetTextWithoutNotify(screenRotationCenter.z.ToString());
+        screenInputFieldZ.onEndEdit.AddListener(value =>
+        {
+            SetScreenRotationCenter(new Vector3(screenRotationCenter.x, screenRotationCenter.y, float.Parse(value)));
+        });
+
+        roomInputFieldX.SetTextWithoutNotify(roomRotationCenter.x.ToString());
+        roomInputFieldX.onEndEdit.AddListener(value =>
+        {
+            SetRoomRotationCenter(new Vector3(float.Parse(value), roomRotationCenter.y, roomRotationCenter.z));
+        });
+        roomInputFieldY.SetTextWithoutNotify(roomRotationCenter.y.ToString());
+        roomInputFieldY.onEndEdit.AddListener(value =>
+        {
+            SetRoomRotationCenter(new Vector3(roomRotationCenter.x, float.Parse(value), roomRotationCenter.z));
+        });
+        roomInputFieldZ.SetTextWithoutNotify(roomRotationCenter.z.ToString());
+        roomInputFieldZ.onEndEdit.AddListener(value =>
+        {
+            SetRoomRotationCenter(new Vector3(roomRotationCenter.x, roomRotationCenter.y, float.Parse(value)));
         });
     }
 
-    private void SetHeight(float height)
+    private void SetScreenRotationCenter(Vector3 center)
     {
-        var localRotationCenterPosition = new Vector3(0, height, 0);
-        if (roomCenter != null)
-        {
-            roomCenter.localPosition = localRotationCenterPosition;
-        }
         if (screenCenter != null)
         {
-            screenCenter.localPosition = localRotationCenterPosition;
+            screenCenter.localPosition = center;
+            this.screenRotationCenter = center;
         }
-        this.height = height;
+    }
+
+    private void SetRoomRotationCenter(Vector3 center)
+    {
+        if (roomCenter != null)
+        {
+            roomCenter.localPosition = center;
+            this.roomRotationCenter = center;
+        }
     }
 }
